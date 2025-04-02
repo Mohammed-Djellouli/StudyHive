1a12c274adef04662d996407d9d9d14584a6b694
Mohammed Djellouli <djelloulimohammed19@gmail.com>
2025-04-02 09:21:22 +0200

    le chat écrit est finis

--- frontend/src/components/Chat/chatBox.js
+++ frontend/src/components/Chat/chatBox.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import MessageList from "./messageList";
 import MessageInput from "./messageInput";
+import socket from "../socket";
 import "./chat.css";
 
 const ChatBox = () => {
     const [messages, setMessages] = useState([]);
 
+
+    useEffect(() => {
+
+        socket.on("receive_message", (message) => {
+            //console.log("message is:", message);
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        return () => {
+            socket.off("receive_message");
+        };
+    }, []);
+
+
     const handleSendMessage = (newMessage) => {
-        setMessages((prevMessages) => [...prevMessages, newMessage]);
+        socket.emit("send_message", newMessage);
     };
 
     return (
--- frontend/src/components/Chat/messageList.js
+++ frontend/src/components/Chat/messageList.js
@@ -1,16 +1,37 @@
 import React from "react";
+import {useEffect,useRef} from "react";
 
 const MessageList = ({ messages }) => {
+    const bottomRef = useRef(null);
+
+    useEffect(() => {
+        bottomRef.current.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
     return (
         <div className="message-list-container">
-            {messages.map((msg, index) => (
-                <div key={index} className="message-row">
-                    <div className="message-list-user">
-                        <img src="/assets/SoloBee.png" className="user-photo"></img>
+            {messages.map((msg, index) => {
+                const isMe = msg.user === "Moi"; //Moi is the username
+                return (
+                    <div
+                        key={index}
+                        className={`message-row ${isMe ? "sent" : "received"}`}
+                    >
+                        {!isMe && (
+                            <div className="message-list-user">
+                                <img src="/assets/SoloBee.png" className="user-photo" alt="abeille" />
+                            </div>
+                        )}
+                        <div className="message-list-message">{msg.text}</div>
+                        {isMe && (
+                            <div className="message-list-user">
+                                <img src="/assets/SoloBee.png" className="user-photo" alt="abeille" />
+                            </div>
+                        )}
                     </div>
-                    <div className="message-list-message">{msg.text}</div>
-                </div>
-            ))}
+                );
+            })}
+            {/*auto scroll into this div*/}
+            <div ref={bottomRef}></div>
         </div>
     );
 };
