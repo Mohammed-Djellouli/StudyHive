dcc4c35995b6d052398b12ba9d187f842f0f7bd4
Mohammed Djellouli <djelloulimohammed19@gmail.com>
2025-04-22 00:17:32 +0200

    commiting the modifications i did on the backend because i made a mistake by commiting the files on the frontend only (same functionalities just modified the model in order to accept string and not only ObjectId and modified the handleHiveCreation so it creats a Hive event the user isn't logged in

--- backend/HandleHiveCreation.js
+++ backend/HandleHiveCreation.js
@@ -1,5 +1,6 @@
 const Hive = require("./models/Hive");
 const User = require("./models/User");
+const {Socket} = require("socket.io");
 
 function generateHiveId(){
     return Math.floor(100000 +Math.random()*900000);
@@ -7,40 +8,66 @@ function generateHiveId(){
 
 
 const HandleHiveCreation = async (req, res) => {
-    try{
-        const {userId , mode } = req.body;
-        if(!userId || !mode){
-            return res.status(400).json({message: "User Id and mode are required"});
+    let guestPseudo;
+    try {
+        const {userId, mode, socketId} = req.body;
+        if (!mode) {
+            return res.status(400).json({message: "Mode is required"});
         }
 
-        const user = await User.findById(userId);
-        if (!user) return res.status(404).json({ message: "User not found." });
-
-        console.log("Utilisateur trouvé :", user);
-
+        //console.log("Utilisateur trouvé :", user);
         const isQueen = mode.toLowerCase() === "queen";
 
+        // if user is connected to his account
+        if (userId) {
+            user = await User.findById(userId);
+            if (!user) return res.status(404).json({message: "User not found."});
+
+            const newHive = new Hive({
+                idRoom: generateHiveId(),
+                createdAt: new Date(),
+                timerEndsAt: new Date(Date.now() + 2 * 60 * 60 * 1000), //The limit time for the hive is two hours
+                isQueenBeeMode: isQueen,
+                idOwner: user._id,
+                users: [user._id],
+            });
+
+            await newHive.save();
+
+            res.status(201).json({
+                message: "User Created Successfully",
+                room: newHive,
+                ownerPseudo: user?.pseudo || "Queen Bee", // fallback si pseudo non défini
+            });
+
+            return
+
+
+        }
+        if (!socketId) {
+            return res.status(400).json({message: "Socket is required"});
+        }
+        //if the user isn't connected to his account
+
+        guestPseudo = `Bee-${Math.floor(1000 + Math.random() * 900)}`;
         const newHive = new Hive({
-            idRoom : generateHiveId(),
+            idRoom: generateHiveId(),
             createdAt: new Date(),
-            timerEndsAt: new Date( Date.now() + 2*60*60*1000), //The limit time for the hive is two hours
+            timerEndsAt: new Date(Date.now() + 2 * 60 * 60 * 1000), //The limit time for the hive is two hours
             isQueenBeeMode: isQueen,
-            idOwner:user._id,
-            users:[user._id],
+            idOwner: socketId,
+            users: [socketId],
         });
 
         await newHive.save();
-
+        console.log(`have created successfully name : ${guestPseudo} , id : ${socketId}`);
         res.status(201).json({
             message: "User Created Successfully",
             room: newHive,
-            ownerPseudo: user?.pseudo || "Queen Bee", // fallback si pseudo non défini
+            ownerPseudo: guestPseudo,
         });
 
-        console.log("User trouvé :", user);
-
-    }
-    catch(err){
+    } catch (err) {
         console.error(err);
         res.status(400).json({message: "Error Creating hive"});
     }
