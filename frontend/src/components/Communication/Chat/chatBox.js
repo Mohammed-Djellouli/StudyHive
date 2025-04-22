c9e090cc5237dc82cddb896f576b2f15c1794346
Mohammed Djellouli <djelloulimohammed19@gmail.com>
2025-04-22 18:36:44 +0200

    fixing some errors to deploy the last commit

--- frontend/src/components/Communication/Chat/chatBox.js
+++ frontend/src/components/Communication/Chat/chatBox.js
@@ -1,4 +1,5 @@
 import React, { useState,useEffect } from "react";
+import {useParams} from "react-router-dom";
 import MessageList from "./messageList";
 import MessageInput from "./messageInput";
 import socket from "../../socket";
@@ -8,6 +9,7 @@ const ChatBox = () => {
     const [messages, setMessages] = useState([]);
     const[userId, setUserId] = useState(null);
     const[socketId, setSocketId] = useState("");
+    const {idRoom: roomId} = useParams();
 
     useEffect(() => {
 
