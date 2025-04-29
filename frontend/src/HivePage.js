beb27f099085f4cafc19bc6ff1fec9b9621c30be
Anis Belaggoun <aniswalidbelaggoun@gmail.com>
2025-04-29 16:00:29 +0200

    feat: Amélioration de l'interface de partage d'écran et de la playlist - Ajout d'une scrollbar personnalisée pour la playlist - Hauteur fixe de la playlist après 3 vidéos - Correction du positionnement de la fenêtre de partage d'écran - Amélioration de la visibilité du partage d'écran

--- frontend/src/HivePage.js
+++ frontend/src/HivePage.js
@@ -14,13 +14,12 @@ import ChatBox from "./components/Communication/Chat/chatBox";
 import VoiceChat from "./components/Communication/MicChat/VoiceChat";
 import BlocNote from "./components/hivePage/hiveBody/BlocNote";
 import WhiteBoard from "./components/hivePage/hiveBody/whiteBoard";
-
-
+import Playlist from "./components/hivePage/hiveBody/videoPlayer/Playlist";
+import VideoContainer from "./components/hivePage/hiveHandle/VideoContainer";
 
 // Nouvelles importations pour les composants modulaires
 import ErrorBoundary from "./components/hivePage/hiveHandle/ErrorBoundary";
 import HiveDataLoader from "./components/hivePage/hiveHandle/HiveDataLoader";
-import VideoContainer from "./components/hivePage/hiveHandle/VideoContainer";
 
 import "./App.css";
 
@@ -35,9 +34,12 @@ function HivePage() {
     const [ownerId, setOwnerId] = useState(null);
     const [users, setUsers] = useState([]);
     const [isScreenShareWindowOpen, setIsScreenShareWindowOpen] = useState(true);
+    // État pour contrôler la visibilité de la playlist
+    const [showPlaylist, setShowPlaylist] = useState(false);
     // Utilisation des hooks personnalisés
     const webRTCFeatures = useWebRTC(idRoom);
     const videoPlayerFeatures = useVideoPlayer(idRoom);
+
     useEffect(() => {
         fetch(`${process.env.REACT_APP_BACKEND_URL}/api/hive/${idRoom}`)
             .then(res => res.json())
@@ -55,7 +57,6 @@ function HivePage() {
             });
     }, [idRoom, location.state]);
 
-
     console.log("State reçu dans HivePage :", ownerPseudo, isQueenBeeMode);
     if(isLoading) {
         return (
@@ -66,59 +67,59 @@ function HivePage() {
     }
     console.log("isInitiator dans HivePage:", webRTCFeatures.isInitiator);
     console.log("isSharing dans HivePage:", webRTCFeatures.isSharing);
+    
     return (
-        <ErrorBoundary>
-            {/*<HiveDataLoader
-                idRoom={idRoom}
-                setOwnerPseudo={setOwnerPseudo}
-                setIsQueenBeeMode={setIsQueenBeeMode}
-                setTimerEndsAt={setTimerEndsAt}
-                setUsers={setUsers}
-                setOwnerId={setOwnerId}
-            />*/}
+        <div className="bg-center bg-cover bg-fixed bg-no-repeat min-h-screen text-white bg-[#1a1a1a]"
+             style={{backgroundImage: "url('/assets/bg.png')", backgroundSize: "270%"}}>
+            <Big_Logo_At_Left/>
+            <Left_bar_Icons_members_In_Room ownerPseudo={ownerPseudo} isQueenBeeMode={isQueenBeeMode}
+                                            users={users.filter((user) => user._id !== ownerId)}/>
 
-            <div className="bg-center bg-cover bg-fixed bg-no-repeat min-h-screen text-white bg-[#1a1a1a]"
-                 style={{backgroundImage: "url('/assets/bg.png')", backgroundSize: "270%"}}>
-                <Big_Logo_At_Left/>
-                <Left_bar_Icons_members_In_Room ownerPseudo={ownerPseudo} isQueenBeeMode={isQueenBeeMode}
-                                                users={users.filter((user) => user._id !== ownerId)}/>
+            <SearchBar onSearch={videoPlayerFeatures.handleSearch}/>
 
-                <SearchBar onSearch={videoPlayerFeatures.handleSearch}/>
+            {/* Main content area with video and playlist */}
+            <div className="flex flex-col items-center relative">
+                {/* Playlist below video */}
+                <div className="w-[850px] mt-4 absolute top-[600px] left-1/2 transform -translate-x-1/2 z-10">
+                    <Playlist onVideoSelect={videoPlayerFeatures.handleVideoSelect} />
+                </div>
 
-                <VideoContainer
-                    webRTCFeatures={webRTCFeatures}
-                    videoPlayerFeatures={videoPlayerFeatures}
-                    isModalOpen={isScreenShareWindowOpen}
-                    setIsModalOpen={setIsScreenShareWindowOpen}
-                />
+                {/* Video player area */}
+                <div className="relative w-full z-20">
+                    <VideoContainer
+                        webRTCFeatures={webRTCFeatures}
+                        videoPlayerFeatures={videoPlayerFeatures}
+                        isModalOpen={isScreenShareWindowOpen}
+                        setIsModalOpen={setIsScreenShareWindowOpen}
+                    />
+                </div>
+            </div>
 
-                {/* Whiteboard Placement
+            {/* Whiteboard Placement
             <div className="fixed top-[100px] left-[200px] z-20">
                 <WhiteBoard />
             </div>
             */}
 
-                <div className="fixed bottom-10 right-4 w-[90vw] max-w-[385px]">
-                    <BlocNote/>
-                    <ChatBox/>
-                </div>
-
-                <div className="fixed bottom-3 right-80">
-                    <LeftBarTools
-                        ownerPseudo={ownerPseudo}
-                        isQueenBeeMode={isQueenBeeMode}
-                        onStartSharing={webRTCFeatures.startSharing}
-                        isInitiator={webRTCFeatures.isInitiator}
-                        isSharing={webRTCFeatures.isSharing}
-                        isScreenShareWindowOpen={isScreenShareWindowOpen}
-                        onToggleScreenShareWindow={() => setIsScreenShareWindowOpen(prev => !prev)}
-                    />
-                </div>
-
-                <HiveTimerBanner ownerPseudo={ownerPseudo} timerEndsAt={timerEndsAt} roomId={idRoom}/>
+            <div className="fixed bottom-10 right-4 w-[90vw] max-w-[385px] z-30">
+                <BlocNote/>
+                <ChatBox/>
             </div>
-        </ErrorBoundary>
-    );
 
+            <div className="fixed bottom-3 right-80 z-30">
+                <LeftBarTools
+                    ownerPseudo={ownerPseudo}
+                    isQueenBeeMode={isQueenBeeMode}
+                    onStartSharing={webRTCFeatures.startSharing}
+                    isInitiator={webRTCFeatures.isInitiator}
+                    isSharing={webRTCFeatures.isSharing}
+                    isScreenShareWindowOpen={isScreenShareWindowOpen}
+                    onToggleScreenShareWindow={() => setIsScreenShareWindowOpen(prev => !prev)}
+                />
+            </div>
+
+            <HiveTimerBanner ownerPseudo={ownerPseudo} timerEndsAt={timerEndsAt} roomId={idRoom}/>
+        </div>
+    );
 }
 export default HivePage;
