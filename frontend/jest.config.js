218e81f9837793034305c79f9e2bdd37ffcd6634
Mohammed Djellouli <djelloulimohammed19@gmail.com>
2025-05-06 23:57:04 +0200

    chat tests

---
+++ frontend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    moduleDirectories: ['node_modules', 'src'],
+    transform: {
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+};
---
+++ frontend/src/components/socketMock.js
@@ -0,0 +1,8 @@
+const socket = {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+    id: 'mock-socket-id',
+};
+
+export default socket;
